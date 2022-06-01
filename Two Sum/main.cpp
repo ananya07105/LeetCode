cd7887925f1e07b083c845fa301d5ba7caa50247
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-06-01 17:48:32 +0530

    Update main.cpp

--- Two Sum/main.cpp
+++ Two Sum/main.cpp
@@ -5,7 +5,8 @@ public:
         unordered_map<int,int> mpp;
         for(int i=0;i<nums.size();i++)
         {
-            if(mpp.find(target-nums[i])!=mpp.end()){
+            if(mpp.find(target-nums[i])!=mpp.end())
+            {
                 ans.push_back(mpp[target-nums[i]]);
                 ans.push_back(i);
                 return ans;
