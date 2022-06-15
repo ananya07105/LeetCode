8125dc0d06a52dfc213ef0bc7444bd0abe3cc93b
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-06-15 21:37:38 +0530

    Update main.cpp

--- Search a 2D matrix/main.cpp
+++ Search a 2D matrix/main.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(!matrix.size()) return false;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int lo=0;
-        int hi=(n*m)-1;
-        while(lo<=hi){
-            int mid=(lo+(hi-lo)/2);
-            if(matrix[mid/m][mid%m]==target)
+        int m=matrix.size(), n=matrix[0].size(),i=0,j=n-1;
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target)
                 return true;
-            if(matrix[mid/m][mid%m]<target)
-                lo=mid+1;
-            else
-                hi=mid-1;
+        
+        else if(matrix[i][j]>target)
+            j--;
+        else 
+            i++;
         }
         return false;
     }
