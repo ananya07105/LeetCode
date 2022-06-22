fb7ecd513f377c507c66a25dd827fc696c59ec3e
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-06-22 23:27:06 +0530

    Create main.cpp

---
+++ Valid Parentheses/main.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> bracket;
+        for(char& c:s){
+            switch(c){
+                case '(':
+                    bracket.push(c);
+                    break;
+                case '{':
+                    bracket.push(c);
+                    break;
+                case '[':
+                    bracket.push(c);
+                    break;
+                case ')':
+                    if(bracket.empty()||bracket.top()!='(')
+                        return false;
+                    else
+                        bracket.pop();
+                    break;
+                case '}':
+                    if(bracket.empty()||bracket.top()!='{')
+                        return false;
+                    else
+                        bracket.pop();
+                    break;
+                case ']':
+                    if(bracket.empty()||bracket.top()!='[')
+                        return false;
+                    else
+                        bracket.pop();
+                    break;
+                    
+                    
+            }
+        }
+        return bracket.empty();
+    }
+};
