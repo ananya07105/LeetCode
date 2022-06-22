f0cbc04d366c5876eff43d07683a52312685d08e
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-06-22 23:47:40 +0530

    Create main.cpp

---
+++ Implement Queue using Stack/main.cpp
@@ -0,0 +1,50 @@
+class MyQueue {
+    
+    std::stack<int> m_normal;
+    std::stack<int> m_reversed;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        m_normal.push(x);
+    }
+    
+    int pop() {
+        if(m_reversed.empty()){
+            while(!m_normal.empty()){
+                m_reversed.push(m_normal.top());
+                m_normal.pop();
+            }
+        }
+        int top=m_reversed.top();
+        m_reversed.pop();
+        return top;
+    }
+    
+    int peek() {
+        if(m_reversed.empty()){
+            while(!m_normal.empty()){
+                m_reversed.push(m_normal.top());
+                m_normal.pop();
+            }
+        }
+        return m_reversed.top();
+        
+    }
+    
+    bool empty() {
+        return m_normal.empty() and m_reversed.empty();
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
