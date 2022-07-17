86ef640f358771874aab87d7d3e3909acf8a39a2
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-07-17 20:28:17 +0530

    Create main.cpp

---
+++ K inverse Pairs Array/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        int mod = 1000000007;
+        if (k > n*(n-1)/2 || k < 0) return 0;
+        vector<vector<long long>> dp(n+1, vector<long long>(k+1, 0));
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
+            if (i + 1 <= n) dp[i + 1][0] = 1;
+            for (int j = 1; j <= min(k, i*(i-1)/2); j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                if (j >= i) dp[i][j] -= dp[i-1][j-i];
+                dp[i][j] = (dp[i][j]+mod) % mod;
+            }
+        }
+        return (int) dp[n][k];
+    }
+};
